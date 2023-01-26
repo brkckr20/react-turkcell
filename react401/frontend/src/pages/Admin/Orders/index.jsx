83f04a003e2fc0079668204d0a5cbdabae438aa7
Burak Çakır <burak2043@gmail.com>
2023-01-26 16:56:43 +0300

    ürünlerin listelenmesi ve silinmesi eklendi

--- react401/frontend/src/pages/Admin/Orders/index.jsx
+++ react401/frontend/src/pages/Admin/Orders/index.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import { fetchOrders } from '../../../api';
+import { Table, Thead, Tbody, Tr, Td, Th, TableCaption, Text } from '@chakra-ui/react'
+import { useEffect } from 'react';
 
 const Orders = () => {
+    const { isLoading, isError, data, error } = useQuery("admin:orders", fetchOrders);
+    if (isLoading) {
+        return <div>loading</div>
+    }
+
+    if (isError) {
+        return <div>{error.message}</div>
+    }
+
     return (
-        <div>Orders</div>
+        <div>
+            <Text fontSize="2xl">Orders</Text>
+            <Table variant="simple">
+                <TableCaption>No data</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>User</Th>
+                        <Th>Address</Th>
+                        <Th>Item</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        data.map(item => (
+                            <Tr key={item._id}>
+                                <Td>{item.user.email}</Td>
+                                <Td>{item.adress}</Td>
+                                <Td isNumeric >{item.items.length}</Td>
+                            </Tr>
+                        ))
+                    }
+
+                </Tbody>
+            </Table>
+        </div>
     )
 }
 
--- react401/frontend/src/pages/Admin/Products/index.jsx
+++ react401/frontend/src/pages/Admin/Products/index.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from 'react-query';
+import { fetchProductList } from '../../../api';
+import { Table } from 'antd';
+import { Link } from 'react-router-dom'
 
 const Products = () => {
+    const { isLoading, isError, data, error } = useQuery("admin:products", fetchProductList);
+    if (isLoading) {
+        return <div>Yükleniyor</div>
+    }
+    if (isError) {
+        return <div>{error.message}</div>
+    }
+
+
+    const columns = [
+        {
+            title: "Title",
+            dataIndex: "title",
+            key: "title"
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            key: "price"
+        },
+        {
+            title: "Created At",
+            dataIndex: "createdAt",
+            key: "createdAt"
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record) => (
+                <>
+                    <Link to={`/admin/products/${record._id}`}>Edit</Link>
+                </>
+            )
+        }
+    ];
     return (
-        <div>Products</div>
+        <div>
+            <p>Products</p>
+            <Table dataSource={data} columns={columns} rowKey={"_id"}>Products</Table>
+        </div>
     )
 }
 
